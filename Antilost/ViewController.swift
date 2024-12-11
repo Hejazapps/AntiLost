46d1e8d219835d5752a947c6833afa4bea743453
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2024-12-11 13:49:07 +0600

    initate

---
+++ Antilost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Antilost
+//
+//  Created by SADIQUL AMIN IBNE AZAD on 11/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AntilostTests/AntilostTests.swift
@@ -0,0 +1,17 @@
+//
+//  AntilostTests.swift
+//  AntilostTests
+//
+//  Created by SADIQUL AMIN IBNE AZAD on 11/12/24.
+//
+
+import Testing
+@testable import Antilost
+
+struct AntilostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
