d3c20a8fa34fc75adee755e6c5a471cccf5726cb
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2024-12-22 15:41:27 +0600

    fixed

--- Cell/AlarmCell.swift
+++ Cell/AlarmCell.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class AlarmCell: UICollectionViewCell {
 
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var imv: UIImageView!
+    @IBOutlet weak var holderView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Vc/AlarmVc.swift
+++ Vc/AlarmVc.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class AlarmVc: UIViewController {
     
+    let soundsArray = [
+      "Air Horn", "Rooster", "Dog", "Whistle", "Gun", "Train",
+      "Police", "Cat", "Alarm", "Door Bell", "Drum",
+      "Baby", "Hello", "Haha", "Ship", "Piano",
+      "Guitar", "Grenade"
+    ];
+    
     var gap:CGFloat = 20
     @IBOutlet weak var collectionViewForAlarm: UICollectionView!
     override func viewDidLoad() {
@@ -45,7 +52,7 @@ extension AlarmVc: UICollectionViewDelegateFlowLayout {
         let bounds = UIScreen.main.bounds
         let width = (bounds.size.width - gap*(numberOfItemsPerRow+2)) / numberOfItemsPerRow
         
-        return CGSize(width: width, height: width + 20)
+        return CGSize(width: width, height: width)
         
         
         // Adjust height if needed
@@ -76,13 +83,19 @@ extension AlarmVc: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return  18
+        return  soundsArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlarmCell", for: indexPath) as!  AlarmCell
+        cell.holderView.addBorder(color: UIColor.lightGray, width: 0.3)
+        cell.holderView.roundCorners(radius: 10.0)
+        cell.imv.image = UIImage(named: "Alarm\(indexPath.row)")
+        cell.lbl.text = soundsArray[indexPath.row]
+        cell.lbl.layer.cornerRadius = 7.0
+        
         
         return cell
     }
@@ -95,3 +108,59 @@ extension AlarmVc: UICollectionViewDataSource {
         
     }
 }
+
+
+
+extension UIView {
+    // Add shadow with customizable properties
+    func addShadow(shadowColor: UIColor = .black,
+                   shadowOpacity: Float = 1,
+                   shadowOffset: CGSize = CGSize(width: 0, height: 1),
+                   shadowRadius: CGFloat = 10.0,
+                   cornerRadius: CGFloat = 10.0) {
+        self.layer.shadowColor = shadowColor.cgColor
+        self.layer.shadowOpacity = shadowOpacity
+        self.layer.shadowOffset = shadowOffset
+        self.layer.shadowRadius = shadowRadius
+        self.layer.cornerRadius = cornerRadius
+        self.layer.masksToBounds = false
+    }
+    
+    
+    class func fromNib(named: String? = nil) -> Self {
+        let name = named ?? "\(Self.self)"
+        guard
+            let nib = Bundle.main.loadNibNamed(name, owner: nil, options: nil)
+        else { fatalError("missing expected nib named: \(name)") }
+        guard
+            /// we're using `first` here because compact map chokes compiler on
+            /// optimized release, so you can't use two views in one nib if you wanted to
+            /// and are now looking at this
+            let view = nib.first as? Self
+        else { fatalError("view of type \(Self.self) not found in \(nib)") }
+        return view
+    }
+    
+    func roundCorners(radius: CGFloat) {
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
+    }
+    
+    
+    // Add border with customizable properties
+    func addBorder(color: UIColor, width: CGFloat) {
+        self.layer.borderColor = color.cgColor
+        self.layer.borderWidth = width
+    }
+    
+    func dropShadow(scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 1
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+}
+
